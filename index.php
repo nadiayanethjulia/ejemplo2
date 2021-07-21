7946c3f3e1bc0ad76059eb0da21145e5f567e398
nadiayanethjulia <87459256+nadiayanethjulia@users.noreply.github.com>
2021-07-21 15:06:31 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?
+$datos= array(
+  array(
+    'id'= 1,
+    'nombre'='Nadia',
+    'edad'=36,
+    'sexo'='Femenino'
+    ), array(
+    'id'= 2,
+    'nombre'='Angel',
+    'edad'=15,
+    'sexo'='Masculino'
+    ),
+  array(
+    'id'= 3,
+    'nombre'='Violeta',
+    'edad'=17,
+    'sexo'='Femenino'
+    ),
+  );
+json_encode($datos)
+  
+    
+?>
