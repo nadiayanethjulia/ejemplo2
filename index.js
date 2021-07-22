ce5080f3887ef4eb6dcad23b4d93c382ace3c3a3
nadiayanethjulia <87459256+nadiayanethjulia@users.noreply.github.com>
2021-07-22 00:22:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 var xhr=new XMLHttpRequest();
 xhr.open('GET', 'index.php');
 xhr.onload= function(){
-  if(xhr.status==200){
+  if(xhr.status === 200){
     var json=JSON.parse(xhr.ResponseText);
-    var template=´´;
-    json.map(function(){
-      tempalte+´
+    var template =``;
+    json.map(function(data){
+      template +=`
       <h2>${data.id}</h2>
        <strong>${data.nombre}</strong>
        <p>${data.edad}</p>
-       <h2>${data.sexo}</p><br>
-      ´;
+       <p>${data.sexo}</p><br>
+      `;
       return template;
     });
-    console.log(template);
+  
     document.getElementById('lista').innerHTML=template;
   }else{
     
     
-  }else{
+ 
     console.log("Existe un error de tipo:"+xhr.status);
   }
 }
+xhr.send();
