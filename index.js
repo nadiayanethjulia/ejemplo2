c03097374610dcea2dc7058d85e8c2073a86b4cd
nadiayanethjulia <87459256+nadiayanethjulia@users.noreply.github.com>
2021-07-21 23:15:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ xhr.onload= function(){
     var template=´´;
     json.map(function(){
       tempalte+´
-      <h2>${json.nombre}</h2>
-      ´
+      <h2>${data.id}</h2>
+       <strong>${data.nombre}</strong>
+       <p>${data.edad}</p>
+       <h2>${data.sexo}</p><br>
+      ´;
+      return template;
     });
+    console.log(template);
+    document.getElementById('lista').innerHTML=template;
+  }else{
+    
     
   }else{
     console.log("Existe un error de tipo:"+xhr.status);
